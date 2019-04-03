3040553f18827ce614e97a8f9f49921ba434dd22
kaushik713 <kaushikv908@gmail.com>
2019-04-03 14:17:49 +0530

    epam task 4

---
+++ datastructre/SelfDrivingBus.java
@@ -0,0 +1,54 @@
+
+/*
+ * self driving bus 
+ */
+package datastructre;
+import java.io.*;
+
+import java.util.*;
+
+public class SelfDrivingBus {
+
+    
+    static int solve(int[][] tree,int n) {
+        int sum=0;
+        for(int i=0;i<tree.length;i++)
+        sum+=tree[i].length;
+
+        sum=sum/2;
+        return (sum+n);
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] tree = new int[n-1][2];
+
+        for (int treeRowItr = 0; treeRowItr < n-1; treeRowItr++) {
+            String[] treeRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int treeColumnItr = 0; treeColumnItr < 2; treeColumnItr++) {
+                int treeItem = Integer.parseInt(treeRowItems[treeColumnItr]);
+                tree[treeRowItr][treeColumnItr] = treeItem;
+            }
+        }
+
+        int result = solve(tree,n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
